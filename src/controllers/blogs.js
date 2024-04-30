74fa0b463f7f6c4040667f1be1024efff16cc4b5
Neam6009 <neeleshnani99@gmail.com>
2024-04-30 19:20:34 +0530

    normally added mongouri

--- src/controllers/blogs.js
+++ src/controllers/blogs.js
@@ -6,11 +6,7 @@ doenv.config({
 	path: "./.env",
 });
 
-MongoDB_URI = process.env.MONGODB_URI;
-
-console.log(process.env.MONGODB_URI)
-
-mongoose.connect(MongoDB_URI, {
+mongoose.connect("mongodb+srv://neelesh:ZgSQfiS6kziYXoIT@propertywaladeployment.dhdxtm8.mongodb.net/WBD_DB?retryWrites=true&w=majority&appName=propertyWalaDeployment", {
 	useNewUrlParser: true,
 	useUnifiedTopology: true
 })
