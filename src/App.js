45725711edabee35013eff27706149146ed53c0b
Neam6009 <neeleshnani99@gmail.com>
2024-04-30 21:45:24 +0530

    *

--- src/App.js
+++ src/App.js
@@ -24,12 +24,12 @@ app.use(jsonParser);
 app.use(csrfProtection);
 
 const corsOptions = {
-  origin: "http://localhost:5173",
+  origin: "*",
   credentials: false,
   optionSuccessStatus: 200,
 };
 
-app.use(cors());
+app.use(cors(corsOptions));
 
 doenv.config({
   path: "./.env",
